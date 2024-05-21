b4974942686340f51196ffdf41b76a864f717290
leomet07 <leomet07@gmail.com>
2024-05-21 12:25:13 -0400

    config to use java

---
+++ Renderer/Ray.java
@@ -0,0 +1,8 @@
+import processing.core.*;
+
+public class Ray {
+    public static void test(){
+        PVector test = new PVector(5, 5);
+        System.out.println(test.mag());
+    }
+}
---
+++ Renderer/Renderer.pde
@@ -0,0 +1,10 @@
+void setup(){
+    size(500, 500);
+    Ray.test();
+}
+
+void draw(){
+    rect(50, 50, 50, 50);
+    fill(255, 0, 0);
+
+}
