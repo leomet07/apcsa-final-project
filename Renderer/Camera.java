3a48865a0f976f37f2822f192c14aa488e518369
leomet07 <leomet07@gmail.com>
2024-05-24 11:01:38 -0400

    all except upper left fixed

--- Renderer/Camera.java
+++ Renderer/Camera.java
@@ -9,26 +9,9 @@ public class Camera {
     int image_height = (int) (image_width / aspect_ratio);
 
     // Camera
-    float focal_length = 1;
-    float viewport_height = 2;
-    float viewport_width = viewport_height * (image_width / image_height);
     public Point eye = new Point(0, 0, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    public PVector viewport_u = new PVector(viewport_width, 0, 0);
-    public PVector viewport_v = new PVector(0, -viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    public PVector pixel_delta_u = PVector.div(viewport_u, image_width);
-    public PVector pixel_delta_v = PVector.div(viewport_v, image_height);
-
-    // Calculate the location of the upper left pixel.
-    public PVector viewport_upper_left = PVector.sub(
-            PVector.sub(PVector.sub(eye, new PVector(0, 0, focal_length)), PVector.div(viewport_u, 2)),
-            PVector.div(viewport_v, 2));
-
-    public PVector pixel00_loc = PVector.add(viewport_upper_left,
-            PVector.mult(PVector.add(pixel_delta_u, pixel_delta_v), (float) 0.5));
+    public Point lookat = new Point(0, 0, -1); // Point camera is looking at
+    public PVector vup = new PVector(0, 1, 0); // Camera-relative "up" direction
 
     public Camera(PApplet pa) {
         this.pa = pa;
@@ -38,6 +21,43 @@ public class Camera {
 
     public void see() {
         Sphere mySphere = new Sphere(new PVector(0, 0, -1), (float) 0.5);
+
+        // Camera stuffs
+        float focal_length = PVector.sub(eye, lookat).mag();
+        double fov = 90; // 90 degrees FOV
+        double theta = fov * Math.PI / 180.0; // radians total of FOV
+        float h = (float) Math.tan(theta / 2); // from center of viewport to top/bottom
+        float viewport_height = 2 * h * focal_length;
+        float viewport_width = viewport_height * (image_width / image_height);
+
+        // // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+        PVector w = PVector.sub(eye, lookat).normalize(null);
+        PVector u = PVector.cross(vup, w, null).normalize(null);
+        PVector v = PVector.cross(w, u, null);
+
+        // // Calculate the vectors across the horizontal and down the vertical viewport
+        // // edges.
+        // vec3 viewport_u = viewport_width * u; // Vector across viewport horizontal
+        // edge
+        PVector viewport_u = PVector.mult(u, viewport_width);
+        // vec3 viewport_v = viewport_height * -v; // Vector down viewport vertical edge
+        PVector viewport_v = PVector.mult(v, -viewport_height);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        // PVector viewport_u = new PVector(viewport_width, 0, 0);
+        // PVector viewport_v = new PVector(0, -viewport_height, 0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        PVector pixel_delta_u = PVector.div(viewport_u, image_width);
+        PVector pixel_delta_v = PVector.div(viewport_v, image_height);
+
+        // Calculate the location of the upper left pixel.
+        PVector viewport_upper_left = PVector.sub(
+                PVector.sub(PVector.sub(eye, new PVector(0, 0, focal_length)), PVector.div(viewport_u, 2)),
+                PVector.div(viewport_v, 2));
+
+        PVector pixel00_loc = PVector.add(viewport_upper_left,
+                PVector.mult(PVector.add(pixel_delta_u, pixel_delta_v), (float) 0.5));
 
         // System.out.println("P3\n" + image_width + " " + image_height + "\n255\n");
         this.pa.loadPixels();
